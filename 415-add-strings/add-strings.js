72e1a5abc3b938fc646085629dc47d0bdc7a1aef
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-04-20 16:21:29 +0530

    Time: 57 ms (67.61%) | Memory: 49.5 MB (77.16%) - LeetSync

---
+++ 415-add-strings/add-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    
+   return String(BigInt(BigInt(num1) + BigInt(num2)))
+
+};
