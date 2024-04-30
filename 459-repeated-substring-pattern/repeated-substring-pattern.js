cd9827f904bb515dca901c44ffca3d84d632e025
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-04-30 11:26:18 +0530

    Time: 53 ms (87.13%) | Memory: 51.4 MB (87.72%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+
+   let str=s+s;
+
+   let newStr=str.substring(1,str.length-1);
+   if(newStr.includes(s)){
+    return true;
+   }else{
+    return false
+   }
+
+    
+};
