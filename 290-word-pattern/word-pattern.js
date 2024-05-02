ee7339d2e5f1146bead3da32fd1ca09bc3267544
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-02 10:51:19 +0530

    Time: 58 ms (18.85%) | Memory: 49.2 MB (12.03%) - LeetSync

---
+++ 290-word-pattern/word-pattern.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+  var map=new Map()
+  let strs=s.split(" ")
+  let PatternSet=[...new Set(pattern.split(""))]
+  let sSet=[...new Set(strs)]
+
+  console.log(PatternSet)
+  console.log(sSet)
+
+  if(pattern.length!=strs.length){
+    return false
+    }
+
+
+    if(PatternSet.length!=sSet.length){
+        return false
+    }
+
+    for(var i=0;i<pattern.length;i++){
+        if(map.has(pattern[i])  && map.get(pattern[i]) !=strs[i]){
+            return false
+        }
+        map.set(pattern[i],strs[i])
+    }
+
+  return true
+
+};
