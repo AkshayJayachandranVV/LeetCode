f0f8ca672f5d9ea516db7ade8340fb88242dafb0
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-02 12:24:26 +0530

    Time: 42 ms (95.73%) | Memory: 48.9 MB (48.22%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -7,14 +7,9 @@ var plusOne = function(digits) {
     let joinValue=digits.join("")
     console.log(joinValue)
     let finalValue=BigInt(joinValue)+1n
-    let output=finalValue.toString().split("").join("")
+    return finalValue.toString().split("")
 
-let array=[]
-    for(i=0;i<output.length;i++){
-        array.push(Number(output[i]))
-    }
 
-    return array
     
   
     
