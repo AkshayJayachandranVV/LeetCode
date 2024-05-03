e1bceba7ed15a5f4069c88c4317e014a291a08cf
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-03 10:00:40 +0530

    Time: 50 ms (69.76%) | Memory: 48.8 MB (53.81%) - LeetSync

--- 461-hamming-distance/hamming-distance.js
+++ 461-hamming-distance/hamming-distance.js
@@ -7,15 +7,11 @@ var hammingDistance = function(x, y) {
 
    let xPos=x.toString(2).split('').reverse()
    let yPos=y.toString(2).split('').reverse()
-   console.log(xPos)
-   console.log(yPos)
-
 
    while(xPos.length<yPos.length) xPos.push('0')
    while(xPos.length>yPos.length) yPos.push('0')
    let count=0
    for(let i=0;i<xPos.length;i++){
-    console.log(xPos[i],yPos[i])
     if(xPos[i]!=yPos[i]){
         count++
     }
