2551b2f687a226153d3d07492fe659c2e068788c
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-06 12:28:30 +0530

    Time: 98 ms (28.12%) | Memory: 52.6 MB (60.74%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    
+    let sortArray=[...score].sort((a,b)=>b-a)
+    console.log(sortArray)
+    let rank=[]
+    for(let i=0;i<sortArray.length;i++){
+        let index=score.indexOf(sortArray[i])
+        if(i==0){
+            rank[index]="Gold Medal"
+        }else if(i==1){
+            rank[index]="Silver Medal"
+        }else if(i==2){
+            rank[index]="Bronze Medal"
+        }else{
+            rank[index]=(i+1).toString()
+        }
+    }
+
+    return rank
+
+};
