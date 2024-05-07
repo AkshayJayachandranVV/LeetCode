383a109f7ed0a569769088af1c6801a4e101ea74
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-07 10:22:45 +0530

    Time: 44 ms (94.02%) | Memory: 49.3 MB (26.25%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    
+    let absentCount=0,late=false
+
+    for(let i=0;i<s.length;i++){
+        if(s[i]=='A'){
+            console.log(s[i])
+           absentCount++
+        }
+        if(absentCount>2){
+            console.log(absentCount)
+            return false
+        }
+
+        if(s[i]=='L' && s[i+1]=='L' && s[i+2]=='L'){
+            late=true
+        }
+    }
+    console.log(late +" "+ absentCount)
+
+    if(late==false && absentCount<2){
+        return true
+    }else{
+        return false
+    }
+
+};
