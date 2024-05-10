49c50e21f53f045c8b5a703600739a63e18d75ec
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-10 09:47:09 +0530

    Time: 57 ms (56.81%) | Memory: 48.9 MB (66.67%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.js
+++ 492-construct-the-rectangle/construct-the-rectangle.js
@@ -4,18 +4,15 @@
  */
 var constructRectangle = function(area) {
 
-    // if(1<=area && area<=10**7){
-
     let sqrt=Math.sqrt(area)
 
     for(let i=Math.floor(sqrt);i>0;i--){
-        console.log(i + " "+ area/i)
         if(area%i==0){
             return [area/i,i]
         }
     }
 
     return [area,1]
-    // }
+
 
 };
