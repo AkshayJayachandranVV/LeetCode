7110c28670fd48f66bea44618577373c2d0c94bc
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-18 09:40:14 +0530

    Time: 91 ms (79.57%) | Memory: 63.2 MB (31.83%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+
+    let count=candyType.length/2
+
+    let setValue=[...new Set(candyType)]
+
+    let diffCandies=setValue.length
+
+    if(count==diffCandies){
+        return count
+    }else if(count<diffCandies){
+        return count
+    }else if(count>diffCandies){
+        return diffCandies
+    }
+    
+};
