3f4542c2c231f939f0cda940c5dc3eb17ada20cd
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-05-31 11:10:01 +0530

    Time: 82 ms (24.41%) | Memory: 53.9 MB (70.87%) - LeetSync

--- 857-positions-of-large-groups/positions-of-large-groups.js
+++ 857-positions-of-large-groups/positions-of-large-groups.js
@@ -4,22 +4,20 @@
  */
 var largeGroupPositions = function(s) {
 
-    let count=1,array=[],start=0
+  let count=1,array=[],start=0
 
-    for(let i=0;i<=s.length;i++){
-        if(s[i]==s[i-1]){
-            count++
-        }else{
-             if(count>=3){
-                array.push([start,i-1])
-             }
-            count=1
-            start=i
+  for(let i=0;i<=s.length;i++){
+    if(s[i]==s[i-1]){
+        count++
+    }else{
+        if(count>=3){
+            array.push([start,i-1])
         }
-    
+        start=i
+        count=1
     }
+  }
 
-
-     return array
+  return array
     
 };
