50fb849af4470eeb8320379d0e13c717307527ee
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-06-03 11:01:36 +0530

    Time: 905 ms (34.77%) | Memory: 77.4 MB (16.10%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    nums=new Set(nums)
+    nums = Array.from(nums);
+    nums.sort((a,b)=>a-b)
+    let count=1,maxCount=0
+
+    for(let i=0;i<nums.length;i++){
+        console.log(nums[i+1] , nums[i])
+        let diff=nums[i+1]-nums[i]
+        if(diff==1){
+            count++
+        }else{
+            if(maxCount<count){
+                maxCount=count
+            }
+            count=1
+        }
+    }
+    return maxCount
+};
