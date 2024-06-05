83adf0fce8821357c8a0dd1a48f2f3cc0d02b0b6
AkshayJayachandranVV <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-06-05 11:38:03 +0530

    Time: 57 ms (80.22%) | Memory: 51.1 MB (80.22%) - LeetSync

--- 1044-find-common-characters/find-common-characters.js
+++ 1044-find-common-characters/find-common-characters.js
@@ -4,21 +4,16 @@
  */
 var commonChars = function(words) {
 
-     let firstWord=words[0],count,output=[]
-    for(let i=0;i<firstWord.length;i++){
-        count=0
-           for(let j=0;j<words.length;j++){
-               if(words[j].includes(firstWord[i])){
-                    count++
-                //   console.log(words)
-               }
-           }
-        //    console.log(count + "" +words.length)
-        if(count==words.length){
-             words=words.map((word)=>word.replace(firstWord[i],""))
-            output.push(firstWord[i])
+     let output=[]
+
+
+     for(let el of words[0]){
+        if(words.every((word)=>word.includes(el))){
+            output.push(el)
+            words=words.map((word)=>word.replace(el,""))
         }
-    }
+     }
+  
 
 
     return output
