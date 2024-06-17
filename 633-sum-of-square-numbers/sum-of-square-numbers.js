40d8bf2b19d4104c9d185bda94d73a8de19e6e62
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-06-17 14:32:41 +0530

    Time: 67 ms (26.03%) | Memory: 49.5 MB (68.49%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+
+    let sum=0
+    let left=0
+    let right=Math.floor(Math.sqrt(c))
+
+    while(left<=right){
+        let sum=left*left+right*right
+
+        if(sum==c){
+            return true
+        }else if(sum>c){
+            right--
+        }else{
+            left++
+        }
+    }
+    return false
+    
+};
