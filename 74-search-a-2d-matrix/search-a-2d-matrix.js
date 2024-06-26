345fe2e7e8bcd32cae74f3d15792de418c4fc2be
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-06-26 09:13:25 +0530

    Time: 57 ms (33.94%) | Memory: 48.9 MB (56.62%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+
+    for(let i=0;i<matrix.length;i++){
+           if(matrix[i].includes(target)){
+            return true
+           }
+    }
+
+    return false
+    
+};
