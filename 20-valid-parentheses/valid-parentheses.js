ab897895f87b889eef20e44ac4006997ddbdcf7a
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-06-28 17:22:58 +0530

    Time: 65 ms (23.24%) | Memory: 50.1 MB (54.11%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+   let stack=[]
+
+   for(let c of s){
+    if(c=="{" || c=="[" || c=="("){
+        stack.push(c)
+    }else{
+        if(c=="}" && stack[stack.length-1]!=="{" ||
+        c=="]" && stack[stack.length-1]!=="[" ||
+        c==")" && stack[stack.length-1]!=="("
+        )
+        return false
+
+        stack.pop()
+    }
+   }
+
+    return stack.length==0
+
+};
