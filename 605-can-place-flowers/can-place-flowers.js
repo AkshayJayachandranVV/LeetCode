a31a1b0b27fabc47e2509a773bb8f885a2b5425c
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-07-01 09:35:26 +0530

    Time: 69 ms (16.99%) | Memory: 50.3 MB (96.62%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -5,16 +5,15 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
     
-    for(let i=0;i<flowerbed.length;i++){
-        let left= i==0 || flowerbed[i-1]==0
-        let right= i==flowerbed.length-1 || flowerbed[i+1]==0
+  for(let i=0;i<flowerbed.length;i++){
+    let left= i==0 || flowerbed[i-1]==0
+    let right= i==flowerbed.length-1 || flowerbed[i+1]==0
 
-        if(left && right && flowerbed[i]==0){
-            flowerbed[i]=1
-            n--
-        }
+    if(left && right && flowerbed[i]==0){
+        flowerbed[i]=1
+        n--
     }
-   
-   return n<=0
+  }
+  return n<=0
 
 };
