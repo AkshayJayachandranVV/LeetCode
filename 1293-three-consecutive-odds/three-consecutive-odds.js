b616edd5f667b110499e85e8875da4dda76994f9
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-07-04 09:12:11 +0530

    Time: 54 ms (44.72%) | Memory: 49 MB (51.83%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function (arr) {
+
+    let count = 0
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] % 2 == 1) {
+            count++
+            if (count >= 3) {
+                return true
+            }
+        } else {
+            count = 0
+        }
+
+    }
+    return false
+
+};
