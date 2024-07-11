bdb71b117b29e0ddefe4d7c331fbaa1b8ddc43af
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-07-11 11:00:34 +0530

    Time: 78 ms (79.27%) | Memory: 54.6 MB (46.45%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+
+    let str="",max=0
+    for(let i=0;i<s.length;i++){
+        if(!str.includes(s[i])){
+           str+=s[i]
+           max=Math.max(max,str.length)
+        }else{
+            let index=str.indexOf(s[i])
+            str=str.substring(index+1) + s[i]
+        }
+    }
+
+    return max
+
+};
