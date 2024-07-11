ec542034d0a6209322897831e18cb0db3abe1c1a
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-07-11 11:07:46 +0530

    Time: 81 ms (70.79%) | Memory: 55.6 MB (32.34%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -5,10 +5,11 @@
 var lengthOfLongestSubstring = function (s) {
 
     let str="",max=0
+
     for(let i=0;i<s.length;i++){
         if(!str.includes(s[i])){
-           str+=s[i]
-           max=Math.max(max,str.length)
+            str+=s[i]
+            max=Math.max(max,str.length)
         }else{
             let index=str.indexOf(s[i])
             str=str.substring(index+1) + s[i]
