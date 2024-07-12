8fbd6ed76685386d3babd78b4c9245fa11969768
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-07-12 09:52:37 +0530

    Time: 74 ms (82.85%) | Memory: 53.6 MB (40.83%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+   function expand(left,right){
+
+        while(left>=0 && right<s.length && s[left]==s[right]){
+            left--
+            right++
+        }
+        return s.substring(left+1,right)
+   }
+
+    let longest=""
+   
+   for(let i=0;i<s.length;i++){
+
+        let odd=expand(i,i)
+        let even=expand(i,i+1)
+
+        if(odd.length>longest.length){
+            longest=odd
+        }
+
+        if(even.length>longest.length){
+            longest=even
+        }
+
+   }
+   return longest
+
+};
