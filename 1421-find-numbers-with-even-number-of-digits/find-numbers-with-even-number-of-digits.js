6fbda52574895e895f2feb4665642a795a8aa598
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-07-31 09:28:37 +0530

    Time: 57 ms (47.22%) | Memory: 50.8 MB (24.29%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -5,9 +5,8 @@
 var findNumbers = function(nums) {
 
     let count=0
-    for(let i=0;i<nums.length;i++){
-        let len = nums[i].toString().length
-        if(len%2==0){
+    for(let i=0;i<nums.length;i++){       
+        if(nums[i].toString().length % 2 == 0){
             count++
         }
     }
