37277174d210d1e6535d578aa1e57ecc3051c048
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-02 12:21:34 +0530

    Time: 1430 ms (5.55%) | Memory: 66.5 MB (23.61%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSwaps = function(nums) {
+
+
+    const n=nums.length
+    const oneSize=nums.reduce((sum,num)=>sum+num,0)
+
+    const part = nums.slice(0,oneSize)
+
+    let onesPart=part.reduce((sum,num)=>sum+num,0)
+
+    let max1s=onesPart
+
+    for(let i=0;i<n;i++){
+         console.log(nums[(oneSize + i) % n] )
+        if(nums[(oneSize + i) % n] ==1){
+            onesPart++
+        }
+
+        if(nums[i] ==1){
+           onesPart--
+        }
+
+        max1s=Math.max(max1s,onesPart)
+    }
+
+    return oneSize - max1s
+
+};
