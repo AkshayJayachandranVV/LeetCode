e3bda8cafa53a890cddfe707f3e5d1b1e5f6b065
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-06 11:04:04 +0530

    Time: 252 ms (12.00%) | Memory: 61.3 MB (20.00%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumPushes = function (word) {
+
+    let map = new Map()
+
+    for (let i = 0; i < word.length; i++) {
+        if (map.has(word[i])) {
+            map.set(word[i], map.get(word[i]) + 1)
+        } else {
+            map.set(word[i], 1)
+        }
+    }
+
+
+    let mapArray = Array.from(map.entries())
+
+    console.log(mapArray)
+
+    let sortArray = mapArray.sort((a, b) => b[1] - a[1])
+
+    let total = 0, value = 0
+
+    for (let j = 0; j < sortArray.length; j++) {
+        if (j % 8 == 0) {
+            value++
+        }
+        total += value * sortArray[j][1]
+    }
+
+    return total
+
+};
