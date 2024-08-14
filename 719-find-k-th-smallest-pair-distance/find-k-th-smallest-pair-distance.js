1a3a768dc8eefa8ed7c53e3caa2964984ca961ea
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-14 11:11:42 +0530

    Time: 56 ms (86.96%) | Memory: 51.5 MB (13.04%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.js
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.js
@@ -5,30 +5,32 @@
  */
 var smallestDistancePair = function(nums, k) {
 
-    nums.sort((a,b)=>a-b)
+  nums.sort((a,b) => a-b)
 
-    let left=0
-    let right=nums[nums.length-1] - nums[0]
+  let left=0
+  let right=nums[nums.length-1] - nums[0]
 
-    while(left<right){
-        const mid= Math.floor((left+right)/2)
-        let count=0
-        let j=0
-        for(let i=0;i<nums.length;i++){
-            while(nums[i]-nums[j]>mid){
-                j++
-            }
-            count+=i-j
+  while(left<right){
+    const mid = Math.floor((left+right)/2)
+
+    let j=0
+    let count=0
+
+    for(let i=0;i< nums.length;i++){
+        while(nums[i]-nums[j]>mid){
+            j++
         }
-
-        if(count<k){
-            left=mid+1
-        }else{
-            right=mid
-        }
-
+    count+=i-j
     }
 
-    return left
+    if(count<k){
+        left=mid+1
+    }else{
+        right=mid
+    }
+
+  }
+
+  return left
     
 };
