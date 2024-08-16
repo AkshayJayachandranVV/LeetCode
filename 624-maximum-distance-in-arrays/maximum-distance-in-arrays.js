1e8ee7927051977050341c041bd61a337c79f851
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-16 10:02:29 +0530

    Time: 113 ms (84.38%) | Memory: 67.1 MB (78.13%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+var maxDistance = function(arrays) {
+
+    let maxDist = 0;
+    let currentMin = arrays[0][0];
+    let currentMax = arrays[0][arrays[0].length - 1];
+
+    for (let i = 1; i < arrays.length; i++) {
+        let minVal = arrays[i][0];
+        let maxVal = arrays[i][arrays[i].length - 1];
+
+        maxDist = Math.max(
+            maxDist,
+            Math.abs(maxVal - currentMin),
+            Math.abs(currentMax - minVal)
+        );
+
+        currentMin = Math.min(currentMin, minVal);
+        currentMax = Math.max(currentMax, maxVal);
+    }
+
+    return maxDist;
+    
+};
