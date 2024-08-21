52cef4827a9b41e8ad3fc92be65ac8b0f77ad2ca
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-21 09:49:07 +0530

    Time: 57 ms (50.07%) | Memory: 49.5 MB (53.82%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    
+    for(let i=0;i<arr.length;i++){
+        let value = arr[i]
+        for(let j=0;j<arr.length;j++){
+            if(arr[i] == 2 * arr[j] && i!==j){
+                return true
+            }
+        }
+    }
+
+    return false
+
+};
