006a3d26fa47823548c4a083bf6d1939a31f2826
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-22 09:41:25 +0530

    Time: 58 ms (37.25%) | Memory: 49.3 MB (25.00%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -4,19 +4,17 @@
  */
 var getNoZeroIntegers = function(n) {
     
+
     let int1 = n-1
     let int2 = 1
 
+    let search = int => int.toString().includes('0')
 
-    const search = int => int.toString().includes('0')
-
-
-    while(search(int1) || search(int2)){
-        int1 = int1 - 1
-        int2 = int2 + 1
-    }
+    while(search(int1) || search(int2) ){
+        int1= int1-1
+        int2= int2+1
+    } 
 
     return [int1,int2]
-     
 
 };
