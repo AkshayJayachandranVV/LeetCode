625d2ab55a940d189eb3605e0496307a7d73c470
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-27 10:50:09 +0530

    Time: 274 ms (5.16%) | Memory: 59.6 MB (14.08%) - LeetSync

---
+++ 1260-day-of-the-year/day-of-the-year.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function(date) {
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let dateArr = date.split("-")
+
+    let days=0
+
+    function isLeapYear(year){
+        if(year % 400 == 0 || (year % 4==0 && year % 100!=0)){
+            return true
+        }
+              return false
+
+    }
+
+
+    console.log(dateArr)
+
+
+    console.log()
+
+    for(let i=0;i<Number(dateArr[1])-1;i++){
+        if(i==1){
+            if(isLeapYear(Number(dateArr[0]))){
+                console.log("enetered")
+                days+=29
+            }else{
+                days+=month[i]
+            }
+        }else{
+            days+=month[i]
+        }
+        
+    }
+
+    return days + Number(dateArr[2])
+    
+};
