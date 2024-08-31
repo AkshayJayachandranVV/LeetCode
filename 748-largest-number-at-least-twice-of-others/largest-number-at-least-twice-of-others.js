c57c42fad9a11c44cdfdaebf1dcc3586e4fae9da
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-08-31 09:22:54 +0530

    Time: 47 ms (84.61%) | Memory: 48.8 MB (74.80%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
@@ -8,14 +8,8 @@ var dominantIndex = function(nums) {
   let largest = Math.max(...nums)
 
   let maxIndex = nums.indexOf(largest)
-
-  console.log(maxIndex)
-    
-
+  
     nums.sort((a,b) => b-a)
-
-    console.log(nums)
-
 
     for(let i=1;i<nums.length;i++){
         if(!(nums[i] * 2 <= largest)){
