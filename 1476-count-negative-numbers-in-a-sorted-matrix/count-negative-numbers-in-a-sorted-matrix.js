455e2f92f55db664a3714d76b4456e722a8761ed
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-09-03 09:45:41 +0530

    Time: 55 ms (59.50%) | Memory: 50 MB (99.83%) - LeetSync

---
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+
+    let count = 0
+    
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]<0){
+                count++
+            }
+        }
+    }
+
+    return count
+    
+};
