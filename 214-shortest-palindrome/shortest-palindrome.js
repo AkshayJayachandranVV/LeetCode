3157bce8fceeb998483c871020de5ef7f318e1d7
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-09-20 10:59:19 +0530

    Time: 778 ms (10.68%) | Memory: 53.7 MB (61.17%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.js
+++ 214-shortest-palindrome/shortest-palindrome.js
@@ -4,10 +4,12 @@
  */
 var shortestPalindrome = function(s) {
 
-    const rev = s.split('').reverse().join('');
-    for (let i = 0; i <= s.length; i++) {
-        if (s.startsWith(rev.slice(i))) {
-            return rev.slice(0, i) + s;
+
+    const rev = s.split("").reverse().join("")
+
+    for(let i=0;i<=s.length;i++){
+        if(s.startsWith(rev.slice(i))){
+            return rev.slice(0,i) + s
         }
     }
     
