ea7f4a3a391dce76da09d2def0550193aafe380d
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-10-03 09:59:59 +0530

    Time: 78 ms (8.57%) | Memory: 51.7 MB (98.78%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
@@ -13,18 +13,19 @@
 var sortedArrayToBST = function(nums) {
 
     const helper = (start,end)=>{
-        if(start>end){
+        
+         if(start>end){
             return null
-        }
+         }
 
-        const mid = Math.floor((start+end)/2)
+         const mid = Math.floor((start+end)/2)
 
-        const root = new TreeNode(nums[mid])
+         const root=new TreeNode(nums[mid])
 
-        root.left= helper(start,mid-1)
-        root.right= helper(mid+1,end)
+         root.left=helper(start,mid-1)
+         root.right=helper(mid+1,end)
 
-        return root
+         return root
     }
 
    return helper(0,nums.length-1)
