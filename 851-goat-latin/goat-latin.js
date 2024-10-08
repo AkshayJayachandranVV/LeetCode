e7b37a21d319509908decaddefc76a9fa109fe3c
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-10-08 10:05:23 +0530

    Time: 90 ms (6.08%) | Memory: 54 MB (7.77%) - LeetSync

---
+++ 851-goat-latin/goat-latin.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function(sentence) {
+
+    let vowels = ['a', 'e', 'i', 'o','u','A','E','I','O','U']
+
+    let append = "ma"
+
+    let str = ""
+
+    let splitValue = sentence.split(" ")
+
+    for(let i=0;i<splitValue.length;i++){
+        append+= "a"
+        console.log(splitValue[i])
+        if(vowels.includes(splitValue[i][0])){
+            let value = splitValue[i].split("")
+            value.push(append)
+            value=value.join("")
+            console.log(value,"------------")
+            str+=value
+        }else{
+            let firstLetter = splitValue[i][0]
+            console.log(firstLetter,"fl")
+            let value = splitValue[i].split("").splice(1,splitValue[i].length)
+            value.push(firstLetter)
+            value.push(append)
+            value=value.join("")
+            console.log(value,"------------")
+            str+=value
+        }
+
+        if (i < splitValue.length - 1) {
+            str += " "; // Add space between words except for the last word
+        }
+    }
+
+    return str
+    
+};
