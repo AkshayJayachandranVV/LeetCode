3f85fa97ca571b4889645675e6f6c2be76cece23
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2024-12-13 11:39:52 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (16.19%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -5,12 +5,14 @@
  */
 var searchInsert = function(nums, target) {
     
-    for(let i=0;i<nums.length;i++){
+  
+   for(let i=0;i<nums.length;i++){
         if(nums[i]>=target){
             return i
         }
-    }
+   }
 
-    return nums.length 
+   return nums.length
+
 
 };
