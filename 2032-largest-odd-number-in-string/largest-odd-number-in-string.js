fa76eaa49859612f8921ace478bad91b688a37b0
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2025-01-21 16:47:45 +0530

    Time: 1 ms (82.23%) | Memory: 52.5 MB (88.32%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+
+   for(let i = num.length - 1;i>=0;i--){
+       if(Number(num[i]%2 !== 0)){
+        return num.slice(0,i+1)
+       }
+   } 
+   return  ""
+
+};
