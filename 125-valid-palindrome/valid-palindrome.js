db6a3026729334ce2a6eb0dd0842e08889369fbd
Akshay Jayachandran  V V <146692000+AkshayJayachandranVV@users.noreply.github.com>
2025-01-29 12:49:05 +0530

    Time: 6 ms (47.22%) | Memory: 52.6 MB (55.92%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,14 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    
+    let replaced = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
 
-   let avoid=s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+    let rev = replaced.split("").reverse().join("")
 
-   let reverse=avoid.split("").reverse().join("")
+    if(replaced == rev){
+        return true
+    }else{
+        return false
+    }
 
-   if(avoid===reverse){
-    return true
-   }else{
-    return false
-   }
 };
